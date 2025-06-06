95731a99f500d4a032e4835acfd39d58a29caf35
Cecilia Lopez <andreacecilopez88@gmail.com>
2025-06-05 23:20:06 -0300

    Added user input and JSON file generation to Main class.

--- src/com/cecilialopez/swapidatafetcher/main/Main.java
+++ src/com/cecilialopez/swapidatafetcher/main/Main.java
@@ -2,11 +2,27 @@ package com.cecilialopez.swapidatafetcher.main;
 
 import com.cecilialopez.swapidatafetcher.services.ConsultaPelicula;
 import com.cecilialopez.swapidatafetcher.models.Pelicula;
+import com.cecilialopez.swapidatafetcher.utils.GeneradorDeArchivo;
+
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Escriba el número de la película de Star Wars que quiere consultar: ");
         ConsultaPelicula consulta = new ConsultaPelicula();
-        Pelicula pelicula = consulta.buscaPelicula(1);
-        System.out.println(pelicula);
+        try {
+            var numeroDePelicula = Integer.valueOf(scanner.nextLine());
+            Pelicula pelicula = consulta.buscaPelicula(numeroDePelicula);
+            System.out.println(pelicula);
+            GeneradorDeArchivo generador = new GeneradorDeArchivo();
+            generador.guardarJson(pelicula);
+        } catch (NumberFormatException e) {
+            System.out.println("Número no encontrado. " + e.getMessage());
+        } catch (RuntimeException | IOException e) {
+            System.out.println(e.getMessage());
+            System.out.println("Finalizando la aplicación.");
+        }
     }
 }
