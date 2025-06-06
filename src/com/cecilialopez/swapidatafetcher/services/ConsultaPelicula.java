ec5a90075f743555a0dd0155174c8447c3ac67a3
Cecilia Lopez <andreacecilopez88@gmail.com>
2025-06-05 23:17:08 -0300

    Improved error handling in ConsultaPelicula and adjusted JSON response parsing.

--- src/com/cecilialopez/swapidatafetcher/services/ConsultaPelicula.java
+++ src/com/cecilialopez/swapidatafetcher/services/ConsultaPelicula.java
@@ -3,12 +3,10 @@ package com.cecilialopez.swapidatafetcher.services;
 import com.cecilialopez.swapidatafetcher.models.Pelicula;
 import com.google.gson.Gson;
 
-import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-
 
 public class ConsultaPelicula {
 
@@ -16,17 +14,13 @@ public class ConsultaPelicula {
         URI direccion = URI.create("https://swapi.py4e.com/api/films/" + numeroDePelicula + "/");
 
         HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(direccion)
-                .build();
+        HttpRequest request = HttpRequest.newBuilder().uri(direccion).build();
         HttpResponse<String> response = null;
         try {
-            response = client
-                    .send(request, HttpResponse.BodyHandlers.ofString());
-        } catch (IOException | InterruptedException e) {
-            throw new RuntimeException(e);
+            response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            return new Gson().fromJson(response.body(), Pelicula.class);
+        } catch (Exception e) {
+            throw new RuntimeException("No encontré esa película.");
         }
-
-        return new Gson().fromJson(response.body(), Pelicula.class);
     }
 }
